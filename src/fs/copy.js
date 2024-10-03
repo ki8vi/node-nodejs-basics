9e5eac04b1c9fb178a7357600179981d17d7d1ac
ki8vi <oneofbln@gmail.com>
2024-10-03 22:15:23 +0500

    feat: implement copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,28 @@
+import { ERROR_MESSAGE } from './utils/constants.js';
+import { determinePathToFile } from './utils/determinePathToFile.js';
+import fs from 'node:fs/promises';
+
+const sourcePath = determinePathToFile(import.meta.url, 'files');
+const destinationPath = determinePathToFile(import.meta.url, 'files_copy');
+
 const copy = async () => {
-    // Write your code here 
+
+    try {
+        const contentOfFolder = await fs.readdir(determinePathToFile(import.meta.url, ''), { withFileTypes: true, recursive: true });
+        const isSourceFolderExist = contentOfFolder.some((el) => el.name === 'files' && el.isDirectory());
+        
+        if(!isSourceFolderExist) {
+            throw new Error(ERROR_MESSAGE);
+        } 
+
+        await fs.cp(sourcePath, destinationPath, { recursive: true, force: false, errorOnExist: true })
+
+    } catch(err) {
+        if(err.code === 'ERR_FS_CP_EEXIST') {
+            throw new Error(ERROR_MESSAGE);
+        }
+        throw err;
+    }
 };
 
 await copy();
