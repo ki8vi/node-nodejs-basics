0a7ca15825e09d5b4813e3218b551cd9fdfc57b0
ki8vi <oneofbln@gmail.com>
2024-10-03 23:52:02 +0500

    feat: implement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,23 @@
+import fs from 'node:fs/promises'; 
+import { determinePathToFile } from './utils/determinePathToFile.js';
+import { TARGET_FOLDER_NAME, ERROR_MESSAGE } from './utils/constants.js';
+
 const list = async () => {
-    // Write your code here 
+    try {
+        const contentOfFolder = await fs.readdir(determinePathToFile(import.meta.url, TARGET_FOLDER_NAME));
+        
+        if(!contentOfFolder) {
+            throw new Error(ERROR_MESSAGE);
+        } 
+
+        console.log(contentOfFolder);
+
+    } catch(err) {
+        if(err.code === 'ENOENT') {
+            throw new Error(ERROR_MESSAGE);
+        }
+        throw err;
+    }
 };
 
 await list();
