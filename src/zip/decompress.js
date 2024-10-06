bd6a7d4e10346b199f3cafa47d0d208ce411e9ea
ki8vi <oneofbln@gmail.com>
2024-10-07 00:09:01 +0500

    feat: implement decompress function

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { resolve } from 'node:path';
+import { pipeline } from 'node:stream/promises';
+import { createGunzip } from 'node:zlib';
+import { getDirname } from '../utils/determinePath.js';
+
+const __dirname = getDirname(import.meta.url);
+const pathToTargetFile = resolve(__dirname, 'files', 'fileToCompress.txt2');
+const pathToCompessedFile = resolve(__dirname, 'files', 'archive.gz');
+
 const decompress = async () => {
-    // Write your code here 
+    try {
+        await pipeline(createReadStream(pathToCompessedFile), createGunzip(), createWriteStream(pathToTargetFile));
+    } catch(err) {
+        throw new Error(err.message);
+    }
 };
 
 await decompress();
